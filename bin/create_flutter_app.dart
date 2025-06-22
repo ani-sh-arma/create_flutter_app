05d19c8d011eb8814d6a42e2504f0ea94e17b66b
Anish <anisharma030@gmail.com>
2025-06-22 23:18:27 +0530

    Initial commit

---
+++ bin/create_flutter_app.dart
@@ -0,0 +1,5 @@
+import 'package:create_flutter_app/create_flutter_app.dart' as create_flutter_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${create_flutter_app.calculate()}!');
+}
